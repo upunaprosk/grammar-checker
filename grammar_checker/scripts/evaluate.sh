28dfd7c765c058dacca2d621f5032e85b522f5c3
upunaprosk <irinaproskurina128@gmail.com>
2023-03-09 22:03:40 +0100

    Update SpaCy version 3.5.0

--- grammar_checker/scripts/evaluate.sh
+++ grammar_checker/scripts/evaluate.sh
@@ -1,4 +1,4 @@
 for i in "$@"
 do
-    python -m spacy evaluate ./training/"$i"/model-best ./realec/"$i"_dev.spacy --output ./metrics/"$i".json --gpu-id 0
+    python -m spacy benchmark accuracy ./training/"$i"/model-best ./realec/"$i"_dev.spacy --output ./metrics/"$i".json --gpu-id 0
 done
