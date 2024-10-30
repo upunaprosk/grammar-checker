d5522fe09936bcd03c8438334d077eba20e32a80
Irina Proskurina <irinaproskurina128@gmail.com>
2024-10-30 21:23:39 +0100

    Refactor code to implement a single language factory

--- grammar_checker/scripts/assemble_pipelines.py
+++ grammar_checker/scripts/assemble_pipelines.py
@@ -7,12 +7,7 @@ def main(loc_output: Path):
     Assemble SpanCategorizer pipelines
     """
     nlp = spacy.blank("en")
-    nlp.add_pipe("punctuation", last=True)
-    nlp.add_pipe("spelling", last=True)
-    nlp.add_pipe("articles", last=True)
-    nlp.add_pipe("grammar_major", last=True)
-    nlp.add_pipe("grammar_minor", last=True)
-    nlp.add_pipe("vocabulary", last=True)
+    nlp.add_pipe("grammar-checker", last=True)
     nlp.to_disk(loc_output)
     return
 
--- grammar_checker/scripts/custom_factories.py
+++ grammar_checker/scripts/custom_factories.py
@@ -9,33 +9,8 @@ meta = get_model_meta(model_path)
 data_dir = f"{meta['lang']}_{meta['name']}-{meta['version']}"
 components_path = model_path / data_dir / "training"
 
-@Language.component("vocabulary")
-def vocabulary(doc):
-    nlp_vocabulary = spacy.load(components_path/"vocabulary")
-    return nlp_vocabulary(doc)
 
-@Language.component("articles")
-def articles(doc):
-    nlp_articles = spacy.load(components_path/"articles")
-    return nlp_articles(doc)
-
-@Language.component("punctuation")
-def punctuation(doc):
-    nlp_punctuation = spacy.load(components_path/"punctuation")
-    return nlp_punctuation(doc)
-
-@Language.component("spelling")
-def spelling(doc):
-    nlp_spelling = spacy.load(components_path/"spelling")
-    return nlp_spelling(doc)
-
-@Language.component("grammar_major")
-def grammar_major(doc):
-    nlp_grammar_major = spacy.load(components_path/"grammar_major")
-    return nlp_grammar_major(doc)
-
-@Language.component("grammar_minor")
-def grammar_minor(doc):
-    nlp_grammar_minor = spacy.load(components_path/"grammar_minor")
-    return nlp_grammar_minor(doc)
-
+@Language.component("grammar_checker")
+def grammar_checker(doc):
+    nlp_grammar = spacy.load(components_path)
+    return nlp_grammar(doc)
